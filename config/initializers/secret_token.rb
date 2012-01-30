f075a139b8a763c461c093cddf34eb5787995d82
Tom Crayford <tcrayford@googlemail.com>
2012-01-30 19:51:58 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Starrys::Application.config.secret_token = '2c888435897203f86058ca45a690ec07cb1a8f82915bda61c099f468a436d07ea636b00f8cc9ad179cc086206ae5c38dc45341f3d66df91c1c0af9987a60b04f'
